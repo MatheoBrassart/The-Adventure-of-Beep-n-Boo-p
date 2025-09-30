9bc2a6632f1c8db059075ad5df74a11be1bb6e3e
MatheoBrassart <matheo.fouineau@brassart.fr>
2025-09-30 10:38:56 +0200

    RessortChange Start

---
+++ the-adventure-of-beep-n'-boop/scripts/ressort_change.gd
@@ -0,0 +1,74 @@
+extends Area2D
+
+class_name RessortChange
+
+# Defines which character the ressort is for. 0 = Beep, 1 = Boop
+@export var WHICH_CHARACTER_IS_IT = 0
+
+const RESSORT_VELOCITY = 900.0
+
+@onready var animatedSpriteRessortChangeBeep: AnimatedSprite2D = $AnimatedSprite2DRessortChangeBeep
+@onready var animatedSpriteRessortChangeBoop: AnimatedSprite2D = $AnimatedSprite2DRessortChangeBoop
+@onready var rightSprite: AnimatedSprite2D = $AnimatedSprite2DRessortChangeBeep
+@onready var collisionShape2D: CollisionShape2D = $CollisionShape2D
+
+@onready var ressortChange2D: Area2D = $"."
+
+var ISACTIVE = 1
+
+func _ready() -> void:
+	
+	# When appearing, hides the incorrect sprite and set the correct one as rightSprite
+	if WHICH_CHARACTER_IS_IT == 0:
+		animatedSpriteRessortChangeBoop.visible = false
+		rightSprite = animatedSpriteRessortChangeBeep
+	else:
+		animatedSpriteRessortChangeBeep.visible = false
+		rightSprite = animatedSpriteRessortChangeBoop
+	
+	# When appearing, check if the right character is active or not
+	var CURRENT_ACTIVE_CHARACTER = get_parent().get_node("Player")
+	if not CURRENT_ACTIVE_CHARACTER.CURRENT_ACTIVE_CHARACTER == WHICH_CHARACTER_IS_IT:
+		deactivate()
+		rightSprite.play("defaultInactive")
+	else:
+		rightSprite.play("defaultActive")
+
+
+func _process(_delta: float) -> void:
+	
+	pass
+
+
+func switch_state():
+	
+	# Called by game_informations
+	if ISACTIVE == 0:
+		activate()
+	else:
+		deactivate()
+
+
+func activate():
+	
+	ISACTIVE = 1
+	rightSprite.play("activating")
+	collisionShape2D.disabled = false
+
+
+func deactivate():
+	
+	ISACTIVE = 0
+	rightSprite.play("deactivating")
+	collisionShape2D.disabled = true
+
+
+func _on_body_entered(body: Node2D) -> void:
+	
+	if body.is_in_group("Player") == true:
+		if ressortChange2D.rotation_degrees == 180:
+			body.velocity.y = RESSORT_VELOCITY * -1
+		elif ressortChange2D.rotation_degrees == -90:
+			#body.direction = 1
+			body.CURRENT_SPEED = RESSORT_VELOCITY
+			body.DECCELERATION_DIRECTION = 1
