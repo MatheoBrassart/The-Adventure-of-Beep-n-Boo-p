1685c0e0091b0f2f7588762921acecbedfe80977
MatheoBrassart <matheo.fouineau@brassart.fr>
2025-10-17 22:34:50 +0200

    Continuing dialogue system

--- the-adventure-of-beep-n'-boop/scripts/dialogue_system.gd
+++ the-adventure-of-beep-n'-boop/scripts/dialogue_system.gd
@@ -1,7 +1,23 @@
-extends CanvasLayer
+extends Control
 
-@onready var characterRichTextLabel: RichTextLabel = $NinePatchRect/Character
-@onready var messageRichTextLabel: RichTextLabel = $NinePatchRect/Message
+@onready var characterRichTextLabel: RichTextLabel = $CanvasLayer/NinePatchRect/Character
+@onready var messageRichTextLabel: RichTextLabel = $CanvasLayer/NinePatchRect/Message
 
-func ChangeTextbox():
-	pass
+var numberOfCharacters: float = 0
+
+@onready var animation_player: AnimationPlayer = $CanvasLayer/NinePatchRect/AnimationPlayer
+
+var DIALOGUE_LIST: Dictionary = {
+	"BeepReveil1" = {"Character": "Beep", "Message": "..."},
+	"BeepReveil2" = {"Character": "Beep", "Message": "Qu... Qu’est-ce qu’il se passe ? Y’a quelqu'un ?"}
+}
+func _process(_delta: float) -> void:
+
+	if Input.is_action_just_pressed("jump"):
+		characterRichTextLabel.text = DIALOGUE_LIST["BeepReveil1"]["Character"]
+		messageRichTextLabel.text = DIALOGUE_LIST["BeepReveil1"]["Message"]
+		
+		numberOfCharacters = DIALOGUE_LIST["BeepReveil1"]["Message"].length()
+		
+		animation_player.speed_scale = 1 + (numberOfCharacters / 2)
+		animation_player.play("scroll")
