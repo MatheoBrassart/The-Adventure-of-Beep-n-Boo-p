3767b3cc7b198b27d00ad2319b1414ee6cb3a3f6
MatheoBrassart <matheo.fouineau@brassart.fr>
2025-10-18 23:07:56 +0200

    OrdiTuto

---
+++ the-adventure-of-beep-n'-boop/scripts/ordi_tuto.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+@onready var animation: AnimatedSprite2D = $Animation
+
+# Defines which tutorial this screen will play
+@export var WHICH_TUTORIAL = "0"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	
+	print("a")
+	if WHICH_TUTORIAL == "move":
+		animation.play("move")
+	elif WHICH_TUTORIAL == "jump":
+		animation.play("jump")
+	elif WHICH_TUTORIAL == "changemoi":
+		animation.play("changemoi")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
