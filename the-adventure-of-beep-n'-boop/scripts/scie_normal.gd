457409d380e17cc47a9211164d039ad550d35f05
MatheoBrassart <matheo.fouineau@brassart.fr>
2026-02-12 11:33:29 +0100

    ScieNormal texture

--- the-adventure-of-beep-n'-boop/scripts/scie_normal.gd
+++ the-adventure-of-beep-n'-boop/scripts/scie_normal.gd
@@ -1,5 +1,12 @@
 extends Area2D
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+
+func _ready() -> void:
+	
+	animated_sprite_2d.play("default")
+
 
 func _on_body_entered(body: Node2D) -> void:
 	
